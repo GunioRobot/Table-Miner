6784398c6be50de4921acfa00abc2ea0b9ce3b66
Gun.io Whitespace Robot <contact@gun.io>
2011-12-18 09:30:33 -0500

    Remove whitespace [Gun.io WhitespaceBot]

--- getCSV.php
+++ getCSV.php
@@ -2,5 +2,5 @@
 header("Content-type: application/octet-stream");
 header("Content-Disposition: attachment; filename=\"my-data.csv\"");
 $data=stripcslashes($_REQUEST['csv_text']);
-echo $data; 
+echo $data;
 ?>
